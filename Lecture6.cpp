54cfe7708b18d7779f610ceb1e8b0bc103a12c64
Prakhar Srivastava <99283212+Prakhar-7@users.noreply.github.com>
2023-09-29 21:20:09 +0530

    Add files via upload

---
+++ Lecture6.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+
+//Binary and Decimal number system
+
+// For generating reverse number use formula: (answer * 10) + digit
+// For generating same sequence number use formula: (digit * 10^i) + answer
+
+// Converting Decimal to Binary
+
+    // int n;
+    // cin >> n;
+
+    // int ans = 0;
+    // int i = 0;
+    // while(n != 0){
+
+    //     int bit = n & 1;
+    //     // cout << "bit " << bit << endl;
+    //     int power = round(pow(10, i));
+    //     // cout <<"power" << power << endl;
+    //     ans = (bit * power) + ans;
+    //     n = n >> 1;
+    //     i++;
+    // } 
+    // cout << "Answer is " << ans << endl;
+
+
+
+// Converting Binary to Decimal
+
+    // int n;
+    // cin >> n;
+
+    // int i = 0, ans = 0;
+    // while( n != 0){
+
+    //     int digit = n % 10;
+    //     if( digit == 1){
+    //         ans = ans + pow(2, i);
+    //     }
+
+    //     n = n/10;
+    //     i++;
+    // }
+    // cout << "Answer is " << ans << endl;
+
+
+//Homework - Negative number to binary
+
+
+}
