735f26577bf025ca27da9b33cc77393bef79ce8c
Prakhar Srivastava <99283212+Prakhar-7@users.noreply.github.com>
2023-09-30 23:47:18 +0530

    Add files via upload

---
+++ Lecture7.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//Leetcode Ques No.7 - To find reverse of given integer.
+
+    // int main(){
+    //     int x;
+    //     cin >> x;
+
+    //     int ans = 0;
+    //     while ( x != 0){
+    //         int digit = x % 10;
+
+    //         if( (ans > INT32_MAX/10) || (ans < INT32_MIN/10)){
+    //             // return 0;
+    //             ans = 0;
+    //             break;
+    //         }
+
+    //         ans = ans * 10 + digit;
+
+    //         x = x/10;
+    //     }
+
+    //     cout << "Reverse is : " << ans << endl;
+    // }
